3a6cca4c959d913461a8c68db6812815653dbae1
Neural0x <alessandro.todde4@gmail.com>
2021-02-12 18:28:33 +0100

    Setted up a basic UI for developing purposes

---
+++ main.py
@@ -0,0 +1,15 @@
+#ThisIsKenny Engine 0.0.1
+#GNU Lesser General Public License
+#Created by 0xNeural
+import os
+import time
+
+def Request():
+    request = input("Domanda: ")
+
+def Start():
+    os.system('clear')
+    print("ThisIsKenny 0.0.1 Developing Mode\n")
+    Request()
+
+Start()
